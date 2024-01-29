00f65e76bc891f06ef1a332e30c0238f350764c3
benyaminsoldier <daniel.benjumea0@gmail.com>
2024-01-29 15:41:42 -0700

    updated the input style with js

--- script.js
+++ script.js
@@ -22,26 +22,42 @@ bodyElement.appendChild(input2)
 bodyElement.appendChild(buttonElement)
 bodyElement.appendChild(sumResult)
 
+let estilo1 = (color)=>{
+    input1.style.border = '1px solid ' + color
+}
+let estilo2 = (color)=>{
+    input2.style.border = '1px solid ' + color
+}
 
 
 
 
 
-
-let validation = (input1, input2, result)=>{
+let validation = (input1, input2, result, estilo1, estilo2)=>{
     try{
         //let inputValue1 = parseInt(input1)
         //let inputValue2 = parseInt(input2)
 
         for(let char in input1)
-            if(isNaN(input1.charAt(char))) throw new Error('Please enter only numeric values')
+            if(isNaN(input1.charAt(char))) {
+                let color = 'red'
+                estilo1(color)
+                throw new Error('Please enter only numeric values')
+            }
         for(let char in input2)
-            if(isNaN(input2.charAt(char))) throw new Error('Please enter only numeric values')
-               
-        //if(isNaN(inputValue1) || isNaN(inputValue2) ) 
+            if(isNaN(input2.charAt(char))) {
+                let color = 'red'
+                estilo2(color)
+                throw new Error('Please enter only numeric values')
+            }
+         
+            
+        
         sumResult.innerHTML = result
     }catch(error){
+        
         sumResult.innerHTML = error.message
+        
     }
 }
 
@@ -50,9 +66,10 @@ buttonElement.addEventListener('click',()=>{
     let value2 = input2.value
     
     let result = parseInt(input1.value) + parseInt(input2.value)
-    validation(value1,value2, result)
+    validation(value1,value2, result, estilo1, estilo2)
     
 })
+
 
 
 
